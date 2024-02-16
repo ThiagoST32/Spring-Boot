45df1b0bed38cacd94e303f72abf3786a99d2741
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2024-02-16 15:11:26 -0400

    Primeiro commit

---
+++ src/main/java/cursospringboot/example/cursospringbootjava/CursoSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package cursospringboot.example.cursospringbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cursospringboot/example/cursospringbootjava/CursoSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cursospringboot.example.cursospringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
